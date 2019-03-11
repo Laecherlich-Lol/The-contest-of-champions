bea1c8ca60c182ee8cdaed8eb8f24c906eefe8b5
Laecherlich-Lol <yiiiiqian@gmail.com>
2019-03-11 20:09:54 +0100

    update

--- Object.py
+++ Object.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 class Node:
     def __init__(self, coordinates):
         self.x = coordinates[0]
@@ -34,6 +37,66 @@ class Object:
         for i, edge in enumerate(self.edges):
             print(" %d: (%.2f, %.2f, %.2f) -> (%.2f, %.2f, %.2f)" % (i, edge.start.x, edge.start.y, edge.start.z, edge.stop.x, edge.stop.y, edge.stop.z))
 
+    def scale(self, n, ref=(0, 0)):
+        for node in self.nodes:
+            node.x = (node.x-1)*n+ref[0]
+            node.y = (node.y-1)*n+ref[1]
+            node.z *= n
+
+    def move(self, direction, distance, object):
+        if direction == 'left':
+            for node in object.nodes:
+                node.x -= distance
+            for edge in object.edges:
+                edge.start.x -= distance
+                edge.stop.x -= distance
+        elif direction == 'right':
+            for node in object.nodes:
+                node.x += distance
+            for edge in object.edges:
+                edge.start.x += distance
+                edge.stop.x += distance
+        elif direction == 'up':
+            for node in object.nodes:
+                node.y -= distance
+            for edge in object.edges:
+                edge.start.y -= distance
+                edge.stop.y -= distance
+        elif direction == 'down':
+            for node in object.nodes:
+                node.y += distance
+            for edge in object.edges:
+                edge.start.y += distance
+                edge.stop.y += distance
+
+    def rotation(self, rec_coords, angle, orig_coords):
+        a, b = rec_coords
+        c0, c1 = orig_coords
+        rho = math.hypot(a-c0, b-c1)
+        phi = math.atan2(b-c1, a-c0) + angle
+        a = rho * math.cos(phi) + c0
+        b = rho * math.sin(phi) + c1
+        return a, b
+
+    def get_center(self):
+        return sum(node.x for node in self.nodes)/len(self.nodes), \
+               sum(node.y for node in self.nodes)/len(self.nodes), \
+               sum(node.z for node in self.nodes)/len(self.nodes)
+
+    def rotate(self, axis, angle=1):
+        center = Node(self.get_center())
+
+        all = ['x', 'y', 'z']
+        all.remove(axis)
+        ele1, ele2 = tuple(all)
+        print(ele1, ele2)
+
+        for node in self.nodes:
+            a, b = self.rotation((getattr(node, ele1), getattr(node, ele2)), angle,
+                                (getattr(center, ele1), getattr(center, ele2)))
+            setattr(node, ele1, a)
+            setattr(node, ele2, b)
+
 
 if __name__ == "__main__":
     cube_nodes = [(x, y, z) for x in (0, 1) for y in (0, 1) for z in (0, 1)]
@@ -42,6 +105,8 @@ if __name__ == "__main__":
     cube.addEdges([(n, n + 4) for n in range(0, 4)])
     cube.addEdges([(n, n + 1) for n in range(0, 8, 2)])
     cube.addEdges([(n, n + 2) for n in (0, 1, 4, 5)])
-
+    cube.outputNodes()
+    cube.outputEdges()
+    cube.rotate_z()
     cube.outputNodes()
     cube.outputEdges()
--- display.py
+++ display.py
@@ -33,6 +33,19 @@ class ProjectionViewer:
                 if event.type == pygame.QUIT:
                     running = False
 
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        for dings in self.objects.values():
+                            # dings.move('up', 20, dings)
+                            dings.rotate('x')
+                    if event.key == pygame.K_DOWN:
+                        for dings in self.objects.values():
+                            # dings.move('up', 20, dings)
+                            dings.rotate('y')
+                    if event.key == pygame.K_LEFT:
+                        for dings in self.objects.values():
+                            # dings.move('up', 20, dings)
+                            dings.rotate('z')
             self.display()
             pygame.display.flip()
 
@@ -40,10 +53,16 @@ class ProjectionViewer:
         """ Draw the objects on the screen. """
 
         self.screen.fill(self.background)
-
+        for dings in self.objects.values():
+            for node in dings.nodes:
+                pygame.draw.circle(self.screen, self.nodeColour, (
+                    round(node.x), round(node.y)), self.nodeRadius)
+            for edge in dings.edges:
+                pygame.draw.line(self.screen, self.edgeColour,
+                                 (round(edge.start.x), round(edge.start.y)), (edge.stop.x, edge.stop.y))
 
 if __name__ == '__main__':
-    pv = ProjectionViewer(400, 300)
+    pv = ProjectionViewer(1000, 1000)
 
     cube = Object.Object()
     cube.addNodes([(x, y, z) for x in (50, 250) for y in (50, 250) for z in (50, 250)])
@@ -51,4 +70,7 @@ if __name__ == '__main__':
         [(n, n + 4) for n in range(0, 4)] + [(n, n + 1) for n in range(0, 8, 2)] + [(n, n + 2) for n in (0, 1, 4, 5)])
 
     pv.addobject('cube', cube)
-pv.run()
+    pv.addobject('cube2', cube)
+    pv.objects['cube'].scale(1)
+    pv.objects['cube'].move('down', 20, pv.objects['cube'])
+    pv.run()
